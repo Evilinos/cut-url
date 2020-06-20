f528c7f9d9e70648c569def080eb773dce00760b
Evilinos <e-streltsov@mail.ru>
2020-06-20 20:14:02 +0300

    '1'

--- app.js
+++ app.js
@@ -5,21 +5,20 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-app.use(express.json({ extended: true }))
+app.use(express.json({extended: true}))
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-const PORT = process.env.PORT ||config.get('port') || 5000
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
+const PORT = process.env.PORT || config.get('port') || 5000
 
 async function start() {
     try {
