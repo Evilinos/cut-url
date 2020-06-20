e494738d333a42408579a3616b666397c21ae40a
Evilinos <e-streltsov@mail.ru>
2020-06-20 18:57:22 +0300

    '1'

---
+++ client/build/precache-manifest.793dc06628340547f0f0e1693e2e489f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e65e6818fc3acd3297427cf76c44d13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba2c0048b44db2f6d6e1",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "6173544b571c17be9a99",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "ba2c0048b44db2f6d6e1",
+    "url": "/static/js/2.f9705d31.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.f9705d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6173544b571c17be9a99",
+    "url": "/static/js/main.f9c48003.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
